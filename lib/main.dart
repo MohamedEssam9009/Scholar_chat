ee3ba0e0e0fb66b6c2b652f011d4fa638d41d3ad
Mohamed Essam <77198018+MohamedEssam-900009@users.noreply.github.com>
2023-12-09 13:43:24 +0200

    add firebase to all platforms

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:chat_app/pages/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 import 'pages/register_page.dart';
 
-void main() {
+void main()async {
+ WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const ScholarChat());
 }
 
