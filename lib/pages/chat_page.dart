ac93e1baed679fd4315719d7f41c76929ae2faf8
Mohamed Essam <77198018+MohamedEssam-900009@users.noreply.github.com>
2023-12-20 23:23:16 +0200

    create app bar for chat page

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/constants.dart';
 import 'package:flutter/material.dart';
 
 class ChatPage extends StatelessWidget {
@@ -7,6 +8,22 @@ class ChatPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: kPrimaryColor,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              kLogo,
+              height: 50.0,
+            ),
+            const Text('Chat'),
+          ],
+        ),
+        centerTitle: true,
+      ),
+    );
   }
 }
