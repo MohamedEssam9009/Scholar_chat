1c39bf390ff0456ee0adff8bd1e0e61cebbd4a34
Mohamed Essam <77198018+MohamedEssam-900009@users.noreply.github.com>
2024-01-06 23:19:57 +0200

    stream builder

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -16,60 +16,70 @@ class ChatPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        backgroundColor: kPrimaryColor,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              kLogo,
-              height: 50.0,
-            ),
-            const Text('Chat'),
-          ],
-        ),
-        centerTitle: true,
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              itemBuilder: (context, index) {
-                return const ChatBubble();
-              },
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextField(
-              controller: controller,
-              onSubmitted: (data) {
-                messages.add({
-                  'message': data,
-                });
-
-                controller.clear();
-              },
-              decoration: InputDecoration(
-                hintText: 'Send Message',
-                suffixIcon: const Icon(
-                  Icons.send,
-                  color: kPrimaryColor,
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(16.0),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(16.0),
-                  borderSide: const BorderSide(color: kPrimaryColor),
-                ),
+    return FutureBuilder<QuerySnapshot>(
+      future: messages.get(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          print(snapshot.data!.docs[0]['message']);
+          return Scaffold(
+            appBar: AppBar(
+              automaticallyImplyLeading: false,
+              backgroundColor: kPrimaryColor,
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    kLogo,
+                    height: 50.0,
+                  ),
+                  const Text('Chat'),
+                ],
               ),
+              centerTitle: true,
             ),
-          ),
-        ],
-      ),
+            body: Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                    itemBuilder: (context, index) {
+                      return const ChatBubble();
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextField(
+                    controller: controller,
+                    onSubmitted: (data) {
+                      messages.add({
+                        'message': data,
+                      });
+
+                      controller.clear();
+                    },
+                    decoration: InputDecoration(
+                      hintText: 'Send Message',
+                      suffixIcon: const Icon(
+                        Icons.send,
+                        color: kPrimaryColor,
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(16.0),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(16.0),
+                        borderSide: const BorderSide(color: kPrimaryColor),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
+        } else {
+          return const Text('Loading.........');
+        }
+      },
     );
   }
 }
