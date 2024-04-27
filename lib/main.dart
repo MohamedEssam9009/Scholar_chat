e88a7806dd0a1e5e1bc1908cb4941832febba164
Mohamed Essam <77198018+MohamedEssam9009@users.noreply.github.com>
2024-04-28 00:48:16 +0300

    Fix some errors

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class ScholarChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       routes: {
         LoginPage.id: (context) => const LoginPage(),
         RegisterPage.id: (context) => const RegisterPage(),
